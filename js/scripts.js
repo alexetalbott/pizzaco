ce32c5f68c23ea3eb2f3cf6a8335912a704493a9
Alex Talbott <alex.e.talbott@gmail.com>
2018-04-04 14:06:38 -0500

    create a working scripts file that shows the chosen crust

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,17 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-      var crust = $("input#radio[name=crust]:checked").val();
-      var size = $("input#radio[name=size]").val();
+      var crust = $("input:radio[name=crust]:checked").val();
+      var size = $("input:radio[name=size]:checked").val();
       var toppings = [];
-      //push toppings into array? for each method?
 
+      //push toppings into array
+      $("input:checkbox[name=new-pizza-topping]:checked").each(function(){
+        toppings.push($(this).val());
+      });
+      var newPizza = new Pizza(crust, size, toppings);
 
+      $("ul#showOrder").append("<li><span class='order'>Crust:"+newPizza.pizzaCrust+"</span></li>");
 
   });
 });
